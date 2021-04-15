98754ccdf667152b9a7c89516f4a6cff012175fe
Kelly Hansen <kellyhansen182@gmail.com>
2021-04-15 16:33:17 -0700

    GameBoard layout

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+function GameBoard(props) {
+  const [gameMatrix, setGameMatrix] = useState([['O', 'X'], [], []]);
+
+  return (
+    gameMatrix.map((row, i) => {
+      return (
+        <div key={'row' + i} className="row">
+          <div className="box">{row[0]}</div>
+          <div className="box">{row[1]}</div>
+          <div className="box">{row[2]}</div>
+        </div>
+      )
+    })
+  );
+}
+
+export default GameBoard;
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import GameBoard from './GameBoard';
 
 function Page() {
   return (
     <div className="page">
       <h1>Tic-Tac-Toe</h1>
       <p>It's PLAYER's turn</p>
+      <GameBoard />
     </div>
   );
 }
