9154008980000d84345718853c6f20ed8403b492
Kelly Hansen <kellyhansen182@gmail.com>
2021-04-15 18:08:46 -0700

    checkForWinner progress

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -4,16 +4,35 @@ function GameBoard(props) {
   const [gameMatrix, setGameMatrix] = useState([[null, null, null], [null, null, null], [null, null, null]]);
 
   function handleBoxClick(e) {
+    if (props.winner) {
+      return;
+    }
     const boxIndex = e.target.getAttribute('data-index').split('');
     if (gameMatrix[boxIndex[0]][boxIndex[1]]) {
       return;
     } else {
+      let newMatrix;
       setGameMatrix(prev => {
         prev[boxIndex[0]][boxIndex[1]] = props.turn;
+        newMatrix = prev;
         return prev;
       });
-      props.toggleTurn();
+      const winner = checkForWinner(newMatrix);
+      if (winner) {
+        props.setWinner(winner);
+      } else {
+        props.toggleTurn();
+      }
     }
+  }
+
+  function checkForWinner(matrix) {
+    for (let i = 0; i < 3; i++) {
+      if (matrix[i][0] && matrix[i].every(val => val === matrix[i][0])) {
+        return matrix[i][0];
+      }
+    }
+    return false;
   }
 
   return (
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -3,6 +3,7 @@ import GameBoard from './GameBoard';
 
 function Page() {
   const [turn, setTurn] = useState('O');
+  const [winner, setWinner] = useState(false);
 
   function toggleTurn() {
     setTurn(prevTurn => prevTurn === 'O' ? 'X' : 'O');
@@ -11,8 +12,8 @@ function Page() {
   return (
     <div className="page">
       <h1>Tic-Tac-Toe</h1>
-      <p>It's {turn}'s turn</p>
-      <GameBoard turn={turn} toggleTurn={toggleTurn} />
+      <p>{winner ? winner + ' Won!' : `It's ${turn}'s turn`}</p>
+      <GameBoard turn={turn} toggleTurn={toggleTurn} winner={winner} setWinner={setWinner} />
     </div>
   );
 }
