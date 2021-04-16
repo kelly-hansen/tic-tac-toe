ca9e16434a8043addfeda3f5846b62be6a8d8684
Kelly Hansen <kellyhansen182@gmail.com>
2021-04-15 17:00:30 -0700

    Set turn state

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GameBoard from './GameBoard';
 
 function Page() {
+  const [turn, setTurn] = useState('O');
+
+  function toggleTurn() {
+    setTurn(prevTurn => prevTurn === 'O' ? 'X' : 'O');
+  }
+
   return (
     <div className="page">
       <h1>Tic-Tac-Toe</h1>
-      <p>It's PLAYER's turn</p>
+      <p>It's {turn}'s turn</p>
       <GameBoard />
     </div>
   );
